bb6c95662c470bf2b860f865d0bc665881198c71
Mish-Al <mtaliep.lifechoices@gmail.com>
2023-09-05 22:08:39 +0200

    store

--- suburban_co/src/store/index.js
+++ suburban_co/src/store/index.js
@@ -9,27 +9,80 @@ export default createStore({
     user: null,
     products: null,
     product: null,
-    
+    categories: [],
+    images: null,
+    brands: null,
+    sizes: null,
+    payment_method: null,
+    reviews: null
   },
   getters: {
+    getCategoryById: (state) => (category_id) => {
+      return state.categories.find((category) => category.category_id === catefory)
+    }
   },
   mutations: {
     setUsers(state, users) {
       state.users = users
     },
-    setUser(state, users) {
-      state.users = users
+    setUser(state, user) {
+      state.users = user
     },
-
+    setProducts(state, products) {
+      state.products = products
+    },
+    setProduct(state, product) {
+      state.products = product
+    },
+    setCategories(state, categories) {
+      state.categories = categories
+    }
+ 
   },
   actions: {
+
+    // <==== User Actions ====>
     async fetchUsers(context) {
       try{
-        const {results} = (await axios.get(`${suburbanUrl}user`)).data
-        context.commit
+        const {results, msg} = (await axios.get(`${suburbanUrl}users`)).data
+        // if(results) {
+        context.commit("setUsers", data.results)
+      // }else {
+      //   sweet({
+      //       title: 'Error',
+      //       text: msg,
+      //       icon: "error",
+      //       timer: 3000
+      //   })
+      // }
+      } catch (e) {
+        context.commit("setMsg", "There was an error");
       }
+    },
+
+
+    // <==== Product Actions ====>
+    async fetchProducts(context) {
+      try {
+        const data = await axios.get(`${suburbanUrl}products`);
+      } catch (e) {
+        alert(e.message);
+      }
+    },
+
+    async fetchProduct(context, product_id) {
+      try {
+        const data = await axios.get(`${suburbanUrl}products/${product_id}`);
+      } catch (e) {
+        alert(e.message);
+      }
+    },
+
+    fetchCategories({ commit }) {
+
     }
-    async login(context, payload)
+    // async addProduct(context, )
+    // // async login(context, payload)
   },
   modules: {
   }
